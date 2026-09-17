45c37f14d90b06c241b3b667497f3b251ff68be9
Alfredo Rocha Molina <alfredorocha0911@gmail.com>
2026-09-17 14:31:39 -0600

    Se agrego la funcion view

--- ProyectoTiendita/src/ControlProductos.java
+++ ProyectoTiendita/src/ControlProductos.java
@@ -17,7 +17,19 @@ public class ControlProductos {
     }
 
     void ver() {
-        throw new UnsupportedOperationException("En construcción."); 
+    System.out.println("\nLISTA DE PRODUCTOS");
+    System.out.println("------------------------------------------");
+    System.out.printf("%-10s %-20s %-10s%n", "CODIGO", "DESCRIPCION", "PRECIO");
+    System.out.println("------------------------------------------");
+
+    for (Producto producto : listaProductos) {
+        System.out.printf("%-10d %-20s $%.2f%n",
+                producto.getCodigo(),
+                producto.getDescripcion(),
+                producto.getPrecio());
+    }
+
+    System.out.println("------------------------------------------");
     }
 
     void buscar() {
