c06ddf180499501df5bfd2b0da284b68af0ed80e
Orlando Salcedo Tonche <orlandosalcedo1510@gmail.com>
2026-09-17 14:22:04 -0600

    Agregue la funcion de modificar

--- ProyectoTiendita/src/ControlProductos.java
+++ ProyectoTiendita/src/ControlProductos.java
@@ -23,6 +23,36 @@ public class ControlProductos {
     void buscar() {
         throw new UnsupportedOperationException("En construcción."); 
     }
-    
-    
+
+    void Modificar(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingresa el ID del producto que deseas modificar: ");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+
+        for (Producto producto : listaProductos) {
+
+            if (producto.getId() == id) {
+
+                System.out.print("Ingresa el nuevo nombre: ");
+                String nuevoNombre = scanner.nextLine();
+
+                System.out.print("Ingresa el nuevo precio: ");
+                float nuevoPrecio = scanner.nextFloat();
+
+                producto.setNombre(nuevoNombre);
+                producto.setPrecio(nuevoPrecio);
+
+                System.out.println("Producto modificado correctamente.");
+                return;
+            }
+        }
+
+        System.out.println("No se encontró un producto con ese ID.");
+    }
 }
+    
+    
+    
+
