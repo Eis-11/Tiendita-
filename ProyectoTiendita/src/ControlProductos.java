828bd3c188d459929d42d8cb2eebd04fe6dfd608
Any Martinez Cerna <ariscerna504@gmail.com>
2026-09-17 14:32:29 -0600

    Se agrega la funcion delete

--- ProyectoTiendita/src/ControlProductos.java
+++ ProyectoTiendita/src/ControlProductos.java
@@ -1,6 +1,7 @@
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ControlProductos {
     
@@ -22,6 +23,21 @@ public class ControlProductos {
 
     void buscar() {
         throw new UnsupportedOperationException("En construcción."); 
+    }
+
+    void eliminar(){
+        Scanner teclado = new Scanner(System.in);
+
+          System.out.println("Ingresa el codigo del producto que deseas eliminar:");
+          int codigo = teclado.nextInt();
+
+          Producto producto = new Producto(codigo, "", 0);
+
+         if (listaProductos.remove(producto)) {
+            System.out.println("Producto eliminado correctamente.");
+            } else {
+                 System.out.println("Producto no encontrado.");
+         }
     }
     
     
